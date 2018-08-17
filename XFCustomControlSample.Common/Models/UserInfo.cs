165e12ae500634e300b9fb7106b616e619b06dd8
Siavash <s.nabavizadeh@IEG-America.com>
2018-08-17 11:50:13 -0400

    Re ordered the solution structure

--- XFCustomControlSample.Common/Models/UserInfo.cs
+++ XFCustomControlSample.Common/Models/UserInfo.cs
@@ -6,6 +6,17 @@ namespace XFCustomControlSample.Common.Models
 {
     public class UserInfo : LoginCredentials
     {
+        public UserInfo(LoginCredentials credentials)
+        {
+            if (credentials != null)
+            {
+                this.Username = credentials.Username;
+                this.Password = credentials.Password;
+            }
+        }
+
+        public UserInfo():this(new LoginCredentials()) { }
+
         public int UserId { get; set; }
 
         public string FirstName { get; set; }
---
+++ XFCustomControlSample.Proxy/Services/LoginService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XFCustomControlSample.Common.Models;
+using XFCustomControlSample.Common.ServiceContracts;
+
+namespace XFCustomControlSample.Proxy.Services
+{
+    public class LoginService : ILoginService
+    {
+        public async Task<ResultPack<UserInfo>> Login(LoginCredentials credentials)
+        {
+            if (credentials == null || string.IsNullOrWhiteSpace(credentials.Username) || string.IsNullOrWhiteSpace(credentials.Password))
+                return new ResultPack<UserInfo> {
+                    IsSucceeded = false,
+                    Message = "Credentials are required"
+                };
+
+            await Task.Delay(1000);
+
+            return 
+                new ResultPack<UserInfo> {
+                    IsSucceeded = true,
+                    ReturnParam = new UserInfo {
+                        FirstName = "John",
+                        LastName = "Doe",
+                        Title = "Mr."
+                    }
+                };
+        }
+    }
+}
